cb4d7208d1d363910fb426331398b1b9aac4182e
Prem Prakash <107941698+XYZcode94@users.noreply.github.com>
2025-02-04 12:02:06 +0000

    WAP to form triangle or not

--- day8.c
+++ day8.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if(year%4==0&&(year%400||year%100)){
+        printf("year is leap year");
+    }else{
+        printf("year is not leap year");
+    }
+}
